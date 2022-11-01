13539f4cf3bf1a1503f7ffad7388fd71090f4ca3
Lucas Phillips <lpquotec@gmail.com>
2022-11-01 15:02:03 -0700

    first commit

---
+++ src/core/Actor.ts
@@ -0,0 +1,18 @@
+import * as PIXI from 'pixi.js';
+export default abstract class Actor extends PIXI.Graphics {
+    update(delta: number) {
+  
+    }
+  
+    onKeydown(e: KeyboardEvent) {
+  
+    }
+  
+    onKeyup(e: KeyboardEvent) {
+  
+    }
+  
+    lerp(start: number, end: number, amt: number) {
+      return (1 - amt) * start + amt * end
+    }
+  }
